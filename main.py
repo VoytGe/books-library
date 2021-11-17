c1e81652214ffc5507a36dcf3e461a14e063b2ef
VoytGe <90923134+VoytGe@users.noreply.github.com>
2021-11-17 15:48:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, NumberRange, URL, ValidationError
 import requests
+import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -13,16 +14,18 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Bootstrap(app)
 
+TODAY_YEAR = datetime.datetime.today().year
+
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    author = db.Column(db.String(250), nullable=True)
+    title = db.Column(db.String(100), unique=True, nullable=False)
+    author = db.Column(db.String(100), nullable=True)
     year = db.Column(db.Integer, nullable=True)
-    isbn_no = db.Column(db.String, nullable=True)
+    isbn_no = db.Column(db.Integer, nullable=True)
     pages = db.Column(db.Integer, nullable=True)
     img_url = db.Column(db.String(250), nullable=True)
-    language = db.Column(db.String(250), nullable=True)
+    language = db.Column(db.String(2), nullable=True)
 
     def __repr__(self):
         return f'<Book {self.title}>'
@@ -34,73 +37,92 @@ class Book(db.Model):
         return dictionary
 
 
+def length(min_len, max_len):
+    message = f'Must include at least {min_len} characters and less than {max_len}'
+
+    def _length(form, field):
+        l = field.data and len(str(field.data)) or 0
+        if l < min_len or l > max_len:
+            raise ValidationError(message)
+
+    return _length
+
+
 class ImportBookForm(FlaskForm):
-    query = StringField("What do you search for:")
+    query = StringField("What do you search for:", validators=[DataRequired(), length(min_len=1, max_len=50)])
     filter = SelectField("Search in:", choices=["everywhere", "title", "author", "subject", "isbn", "lccn", "oclc"])
     submit = SubmitField("Search")
 
 
 class AddBook(FlaskForm):
-    title = StringField("Book Title", validators=[DataRequired()])
-    author = StringField("Author", validators=[DataRequired()])
-    year = IntegerField("Publishing year", validators=[DataRequired()])
-    isbn_no = StringField("ISBN number", validators=[DataRequired()])
-    pages = IntegerField("No. of pages", validators=[DataRequired()])
-    img_url = StringField("Cover image", validators=[DataRequired()])
-    language = StringField("Language", validators=[DataRequired()])
+    title = StringField("Book Title", validators=[DataRequired(), length(min_len=1, max_len=100)])
+    author = StringField("Author", validators=[DataRequired(), length(min_len=1, max_len=100)])
+    year = IntegerField("Publishing year", validators=[DataRequired(message="Provide numbers"),
+                                                       length(min_len=4, max_len=4),
+                                                       NumberRange(min=1000, max=TODAY_YEAR)])
+    isbn_no = IntegerField("ISBN number", validators=[DataRequired(message="Provide numbers"),
+                                                      length(min_len=13, max_len=13),
+                                                      NumberRange(min=0)])
+    pages = IntegerField("No. of pages", validators=[DataRequired(message="Provide numbers"),
+                                                     length(min_len=1, max_len=6),
+                                                     NumberRange(min=1, max=100000)])
+    img_url = StringField("Cover image", validators=[DataRequired(), URL()])
+    language = StringField("Language", validators=[DataRequired(), length(min_len=2, max_len=2)])
+    submit = SubmitField("Add/Edit Book")
+
+
+class SearchBook(FlaskForm):
+    title = StringField()
+    author = StringField()
+    language = StringField()
+    year_from = IntegerField()
+    year_to = IntegerField()
     submit = SubmitField("Add/Edit Book")
 
 
 db.create_all()
 
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    if request.method == "POST":
-        if request.form['year_from'] == '':
-            year_from = 0
-        else:
-            year_from = int(request.form['year_from'])
-        if request.form['year_to'] == '':
-            year_to = 3000
-        else:
-            year_to = int(request.form['year_to'])
-        qry = db.session.query(Book)\
-            .filter(Book.title.contains(request.form["title"]))\
-            .filter(Book.author.contains(request.form["author"]))\
-            .filter(Book.language.contains(request.form["lang"]))\
-            .filter(Book.year >= year_from)\
-            .filter(Book.year <= year_to)\
-            .all()
-    else:
-        qry = Book.query.all()
-
-    return render_template("index.html", books=qry)
+def custom_year(start_year, end_year):
+    year = start_year
+    if start_year == "":
+        year = end_year
+    return int(year)
 
 
-@app.route("/search", methods=["GET"])
-def search_book():
-    title = request.args.get("title")
-    author = request.args.get("author")
-    lang = request.args.get("lang")
-    if request.args.get("from") == '':
-        year_from = 0
-    else:
-        year_from = int(request.args.get("from"))
-    if request.args.get("to") == '':
-        year_to = 3000
-    else:
-        year_to = int(request.args.get("to"))
-    qry = db.session.query(Book) \
-        .filter(Book.title.contains(title)) \
-        .filter(Book.author.contains(author)) \
-        .filter(Book.language.contains(lang)) \
-        .filter(Book.year >= year_from) \
-        .filter(Book.year <= year_to) \
+def get_search_data(book_qry):
+    title = book_qry.get("title")
+    author = book_qry.get("author")
+    lang = book_qry.get("lang")
+    year_from = custom_year(start_year=book_qry.get("from", 1000), end_year=1000)
+    if year_from == "":
+        year_from = 1000
+    year_to = custom_year(start_year=book_qry.get("to", TODAY_YEAR), end_year=TODAY_YEAR)
+    qry = db.session.query(Book)\
+        .filter(Book.title.contains(title))\
+        .filter(Book.author.contains(author))\
+        .filter(Book.language.contains(lang))\
+        .filter(Book.year >= year_from)\
+        .filter(Book.year <= year_to)\
         .all()
-    if qry:
-        qry_book = [book.to_dict() for book in qry]
-        return jsonify(book=qry_book)
+    return qry
+
+
+@app.route("/")
+def home():
+    if request.args:
+        lib_books = get_search_data(request.args)
+    else:
+        lib_books = Book.query.all()
+    return render_template("index.html", books=lib_books)
+
+
+@app.route("/search")
+def search_book():
+    books = get_search_data(request.args)
+    if books:
+        lib_books = [book.to_dict() for book in books]
+        return jsonify(book=lib_books)
     else:
         return jsonify(error={"Not Found": "There is no such book in the library"})
 
@@ -119,13 +141,13 @@ def add_book():
             book_to_update.language = form.language.data
         else:
             new_book = Book(
-                title=form.title.data,
-                author=form.author.data,
+                title=form.title.data.title(),
+                author=form.author.data.title(),
                 year=form.year.data,
                 isbn_no=form.isbn_no.data,
                 pages=form.pages.data,
                 img_url=form.img_url.data,
-                language=form.language.data
+                language=form.language.data.lower()
             )
             db.session.add(new_book)
         db.session.commit()
@@ -159,6 +181,7 @@ def import_book():
 
         parameters = {
             "key": "AIzaSyDe7j8Fuk0ffmNDzYPyCOsOhhpKeSnqD2Q",
+            "maxResults": 40,
             "q": qry
         }
         response = requests.get(url="https://www.googleapis.com/books/v1/volumes", params=parameters)
@@ -175,42 +198,32 @@ def import_book():
 @app.route("/find/<book_id>", methods=["GET", "POST"])
 def find_book(book_id):
     response = requests.get(url=f"https://www.googleapis.com/books/v1/volumes/{book_id}",
-                            params={"key": "AIzaSyDe7j8Fuk0ffmNDzYPyCOsOhhpKeSnqD2Q"})
+                            params={
+                                "key": "AIzaSyDe7j8Fuk0ffmNDzYPyCOsOhhpKeSnqD2Q",
+                                "maxResults": 40
+                            })
     data = response.json()["volumeInfo"]
-    for book in Book.query.all():
-        if data["title"] == book.title:
-            msg = "The book is already in the library."
-            return render_template("notification.html", msg=msg)
 
-    book_params = ["title", "authors", "publishedDate", "industryIdentifiers", "pageCount", "imageLinks", "language"]
-    book_set = {}
-    for item in book_params:
-        if item in data:
-            book_set[item] = data[item]
-        else:
-            book_set[item] = ''
-    isbn_no = [isbn['identifier'] for isbn in book_set["industryIdentifiers"] if isbn['type'] == 'ISBN_13']
-    publish_year = book_set["publishedDate"].split("-")[0]
-    if book_set["imageLinks"] != '':
-        book_img = book_set["imageLinks"]["smallThumbnail"]
+    if Book.query.filter_by(title=data["title"]).first():
+        msg = "The book is already in the library."
+        return render_template("notification.html", msg=msg)
     else:
-        book_img = book_set["imageLinks"]
+        isbn_no = [isbn['identifier'] for isbn in data.get("industryIdentifiers") if isbn['type'] == 'ISBN_13']
 
-    new_book = Book(
-        title=book_set["title"],
-        author=book_set["authors"][0],
-        year=publish_year,
-        isbn_no=isbn_no[0],
-        pages=book_set["pageCount"],
-        img_url=book_img,
-        language=book_set["language"]
-    )
+        new_book = Book(
+            title=data.get("title").title(),
+            author=data.get("authors")[0].title(),
+            year=data.get("publishedDate").split("-")[0],
+            isbn_no=isbn_no[0],
+            pages=data.get("pageCount"),
+            img_url=data.get("imageLinks").get("smallThumbnail"),
+            language=data.get("language").lower()
+        )
 
-    db.session.add(new_book)
-    db.session.commit()
-    return redirect(url_for("home"))
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for("home"))
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    # unittest.main()
